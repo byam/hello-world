06a6262c316d4178ef0fd3c5d528c5ce684acd48
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2018-12-24 01:20:49 +0900

    updated

--- java/maven/my-app/src/main/java/com/my/app/Main.java
+++ java/maven/my-app/src/main/java/com/my/app/Main.java
@@ -1,4 +1,30 @@
 package com.my.app;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Main {
+    static final String GREETING = "Merry Christmas!";
+
+    public static void main (String args[]) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            System.out.println("Server running...");
+            t.sendResponseHeaders(200, GREETING.length());
+            OutputStream outputStream = t.getResponseBody();
+            outputStream.write(GREETING.getBytes());
+            outputStream.close();
+        }
+    }
 }
