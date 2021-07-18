82eb011530cb97259890deaeba6cdf1492dfdafa
bya-ganbaatar <bya_ganbaatar@r.recruit.co.jp>
2021-07-18 16:49:31 +0900

    go

---
+++ parallel-computing/go/hello.go
@@ -0,0 +1,20 @@
+package hello
+
+import (
+	"fmt"
+	"time"
+)
+
+func printHello() {
+	fmt.Println("Hello")
+}
+
+func printWorld() {
+	fmt.Println("World")
+}
+
+func main() {
+	printHello()
+	printWorld()
+	time.Sleep(time.Second)
+}
